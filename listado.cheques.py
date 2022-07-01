02e0f219c2aa45ace19dbeabf449611282ea622a
Daniel Valbuena <danielval0708@gmail.com>
2022-07-01 00:00:40 -0300

    cambios en el csv

--- listado.cheques.py
+++ listado.cheques.py
@@ -6,3 +6,4 @@ with open('cheques.csv') as f: # lo que estamos haciendo aqui es iterar linea po
         print("{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}".format(row[0],row[1],row[2],row[3],row[4],row[5],row[6],row[7],row[8],row[9],row[10]))
 
 
+
